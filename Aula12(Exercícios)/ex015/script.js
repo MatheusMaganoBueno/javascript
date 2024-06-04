fc8937e8588f0f36b751ca575bb4c257c015eb59
Matheus Magano Bueno <142444039+MatheusMaganoBueno@users.noreply.github.com>
2024-06-03 23:37:15 -0300

    Módulo D

--- Aula12(Exercícios)/ex015/script.js
+++ Aula12(Exercícios)/ex015/script.js
@@ -3,18 +3,15 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${horas} horas.`
-
+    msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!!
-        img.scr = 'manha.png'
-        document.body.style.background = '#e2cd9f'
+        img.scr ='../imagens/manha.png'
     } else if(hora >= 12 && hora < 18) {
         //BOR TARDE!!
-        img.scr = 'tarde.png'
-        document.body.style.background = '#e2cd9f'
+        img.scr ='../imagens/tarde.png'
     } else {
-        img.scr = 'noite.png'
-        document.body.style.background = '#e2cd9f'
+        img.scr ='../imagens/noite.png'
     }
+
 }
