b13a38d8fe7681288cc93dbf72bf9bc562e15b74
Matheus Magano Bueno <142444039+MatheusMaganoBueno@users.noreply.github.com>
2024-06-05 20:50:19 -0300

    Módulo D

--- Aula12(Exercícios)/ex015/javascript/script.js
+++ Aula12(Exercícios)/ex015/javascript/script.js
@@ -6,13 +6,9 @@ function  carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 6 && hora < 12) {
-        //BOM DIA!!
-        console.log(hora)
         document.body.style.background = '#FDDFC1'
         img.scr = './manha.png'
     } else if(hora >= 12 && hora < 18) {
-        //BOR TARDE!!
-        console.log(hora)
         document.body.style.background = '##E5B59E'
         img.scr = './tarde.png'
     } else {
--- Aula12(Exercícios)/ex016/javascript/script.js
+++ Aula12(Exercícios)/ex016/javascript/script.js
@@ -1,1 +1,18 @@
+function verificar() {
+    var anonascimento = window.document.querySelector('#iano')
+    var ano = parseInt(anonascimento);
+    var mas = window.document.querySelector('#imas')
+    var fem = window.document.querySelector('#ifem')
+    var foto = window.document.querySelector('#imagem')
+    var msg = window.document.querySelector('#imsg')
 
+    switch(gender) {
+        case (mas = true && ano <= 2024 && ano > 2021):
+        foto.scr = './agua.jpg'
+        break;
+
+        default: 
+        console.log('Olá mundo')
+        break;
+    } 
+}
