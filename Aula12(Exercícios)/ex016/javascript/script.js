ab1698e19f322d04a906191f344a9f781b939eb8
Matheus Magano Bueno <142444039+MatheusMaganoBueno@users.noreply.github.com>
2024-06-07 17:24:40 -0300

    Módulo E

--- Aula12(Exercícios)/ex016/javascript/script.js
+++ Aula12(Exercícios)/ex016/javascript/script.js
@@ -3,6 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano =  document.getElementById('txtano')
     var res = document.getElementById('res')
+
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamento!')
     } else {
@@ -11,6 +12,7 @@ function verificar() {
         var gênero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+        
         if (fsex [0].checked) {
             gênero = 'Homem'
             if(idade >= 0 && idade < 3) {
