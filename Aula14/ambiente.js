c1130e1bfe62b4e9cdcfae83902d3d00dfd10c9e
Matheus Magano Bueno <142444039+MatheusMaganoBueno@users.noreply.github.com>
2024-06-07 21:09:09 -0300

    Módulo E

---
+++ Aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while(c <= 5) {
+    console.log(c)
+    c++ ou c = c + 1
+}
+*/
+
+/* Códigos Iguais Usando Propriedades Diferentes
+*/
+
+for(c=1;c<=5;c++){
+    console.log(`Passo ${c}`)
+}
